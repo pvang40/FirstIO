08c3156bb3619df55db187c8e7c8cb93e0c1e97c
pvang40 <pvang40@mail.csuchico.edu>
2018-01-29 17:11:38 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+// Author: Pachia Vang 
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hello, my name is Pachia!" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
